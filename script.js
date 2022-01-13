a58ddb53c855d82abd66637079ceb7810ae22a21
Hayato Kadowaki <h-kadowaki@vectorinc.co.jp>
2022-01-13 18:16:44 +0900

    update: date data, expired

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ window.addEventListener('DOMContentLoaded', function () {
     countDown();
 });
 
-/**
- * カウントダウンの目標日付
- * @type {object}
- */
 const futureDate = {
     year: 2022,
-    month: 0, // 1月を0として計算する 5月 -> 4
-    date: 24,
+    month: 5,
+    date: 5,
     hours: 0,
     minutes: 0,
     seconds: 0,
 };
 
 function countData() {
-    const countDownDate = new Date(futureDate.year, futureDate.month, futureDate.date, futureDate.hours, futureDate.minutes, futureDate.seconds).getTime();
+    const countDownDate = new Date(futureDate.year, futureDate.month - 1, futureDate.date, futureDate.hours, futureDate.minutes, futureDate.seconds).getTime();
     const now = new Date().getTime();
     const distance = countDownDate - now;
+    if (distance < 0) {
+        // 期限切れた場合
+        return false;
+    }
     const date = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -33,19 +33,36 @@ function countData() {
 }
 
 function countDown() {
+    const timerElement = document.querySelector('[data-count="timer"]');
+    const expiredElement = document.querySelector('[data-count="expired"]');
     const dateElement = document.querySelector('[data-count="date"]');
     const hoursElement = document.querySelector('[data-count="hours"]');
     const minutesElement = document.querySelector('[data-count="minutes"]');
     const secondsElement = document.querySelector('[data-count="seconds"]');
-    setInterval(() => {
-        const data = countData();
-        dateElement.textContent = addZero(data.date);
-        hoursElement.textContent = addZero(data.hours);
-        minutesElement.textContent = addZero(data.minutes);
-        secondsElement.textContent = addZero(data.seconds);
+    setData(true);
+    const interval = setInterval(() => {
+        setData();
     }, 1000);
+    function setData(init) {
+        const data = countData();
+        if (!data) {
+            if (!init) {
+                clearInterval(interval);
+            }
+            timerElement.classList.add('is-hidden');
+            expiredElement.classList.add('is-active');
+        } else {
+            dateElement.textContent = addZero(data.date);
+            hoursElement.textContent = addZero(data.hours);
+            minutesElement.textContent = addZero(data.minutes);
+            secondsElement.textContent = addZero(data.seconds);
+        }
+    }
 
     function addZero(number) {
-        return (Array(2).join('0') + number).slice(-2);
+        if (String(number).length < 3) {
+            return (Array(2).join('0') + number).slice(-2);
+        }
+        return number;
     }
 }
